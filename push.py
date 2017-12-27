a6851c59af9bd6156953f74fd3dc920c38c5cbaf
hengyue <305069590@qq.com>
2017-12-27 16:21:24 +0900

    2017-12-27 16:21:24

---
+++ push.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import datetime,os
+
+
+
+time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+gitadd = "git add .;"
+gitcom = "git commit -m "+"\""+time+"\";"
+gitpus = "git push -u origin master --force;"
+
+os.system(  gitadd + gitcom + gitpus )
